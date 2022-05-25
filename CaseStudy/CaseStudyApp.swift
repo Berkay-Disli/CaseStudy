112913cad5d15d7a52cb6a5446c6d1ba12fdd00c
Berkay-Disli <berkaydisli.work@gmail.com>
2022-05-25 14:26:28 +0300

    Initial Commit

---
+++ CaseStudy/CaseStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseStudyApp.swift
+//  CaseStudy
+//
+//  Created by Berkay Disli on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
